ba45caa17428c610451048b40fce1106678c6c54
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2026-02-08 13:30:50 -0300

    Testando projeto

--- src/test/java/junit/lucas/CalculadoraTest.java
+++ src/test/java/junit/lucas/CalculadoraTest.java
@@ -7,6 +7,7 @@ public class CalculadoraTest {
 	@Test
 	public void testSomar() {
 	Calculadora calc =  new Calculadora();
+	System.out.println(calc.soma(2, 3) == 5);
 	Assertions.assertTrue(calc.soma(2, 7) == 5);
 	}
 	
