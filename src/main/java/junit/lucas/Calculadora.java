1f5444e9d37b0221cf128083665060cb3f137d21
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2026-02-08 13:27:40 -0300

    Primeiro commit do projeto

---
+++ src/main/java/junit/lucas/Calculadora.java
@@ -0,0 +1,17 @@
+package junit.lucas;
+
+public class Calculadora {
+	
+	public int soma(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		Calculadora calc =  new Calculadora();
+		calc.soma(2, 3);
+		System.out.println(calc.soma(2, 3) == 5);
+		System.out.println(calc.soma(3, 4) == 7);
+		System.out.println(calc.soma(5, 6) == 11);
+	}
+	
+}
---
+++ src/test/java/junit/lucas/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package junit.lucas;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+	
+	@Test
+	public void testSomar() {
+	Calculadora calc =  new Calculadora();
+	Assertions.assertTrue(calc.soma(2, 7) == 5);
+	}
+	
+
+}
